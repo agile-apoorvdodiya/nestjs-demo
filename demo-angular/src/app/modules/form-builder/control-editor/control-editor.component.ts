a2187b0a712f50da7df41e0d195a4d3b22e54290
agile-apoorvdodiya <apoorva.h@agileinfoways.com>
2022-05-12 13:07:57 +0530

    Form editing implemented

--- demo-angular/src/app/modules/form-builder/control-editor/control-editor.component.ts
+++ demo-angular/src/app/modules/form-builder/control-editor/control-editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-control-editor',
@@ -11,15 +12,50 @@ export class ControlEditorComponent implements OnInit {
   @Input('display') set setDisplay(value: boolean) {
     this.display = value === true ? 'block' : 'none';
   }
-  constructor() {}
+  @Input('data') data: any;
+  formValue: any;
+  @Input('index') index: number | null = null;
 
-  ngOnInit(): void {}
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    console.log(this.data);
+    this.formValue = this.data?.value;
+  }
 
   onCloseHandled() {
     this.display = 'none';
     this.closeEvent.emit({
       close: true,
-      data: {},
+      save: false,
+      data: { index: this.index },
     });
+  }
+
+  onSave() {
+    this.display = 'none';
+    this.closeEvent.emit({
+      close: false,
+      save: true,
+      data: { index: this.index, form: this.data?.value || {} },
+    });
+  }
+
+  getControlsArray() {
+    return (this.data.controls['controls'] as FormArray)
+      .controls as FormGroup[];
+  }
+
+  deleteControl(index: number) {
+    (this.data.controls['controls'] as FormArray).removeAt(index);
+  }
+
+  addControl() {
+    (this.data.controls['controls'] as FormArray).push(
+      this.fb.group({
+        label: '',
+        value: '',
+      })
+    );
   }
 }
--- demo-angular/src/app/modules/form-builder/create-form/create-form.component.ts
+++ demo-angular/src/app/modules/form-builder/create-form/create-form.component.ts
@@ -5,14 +5,17 @@ import {
   copyArrayItem,
 } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, Validators } from '@angular/forms';
-
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { Modal } from "bootstrap";
 @Component({
   selector: 'app-create-form',
   templateUrl: './create-form.component.html',
   styleUrls: ['./create-form.component.scss'],
 })
 export class CreateFormComponent implements OnInit {
+  openModal = false;
+  selectedControl: any = null;
+  index: null | number = null;
   constructor(private fb: FormBuilder) {}
   ngOnInit(): void {
     // this.updateForm();
@@ -81,9 +84,20 @@ export class CreateFormComponent implements OnInit {
       (this.form.controls['form'] as FormArray).insert(
         event.currentIndex,
         this.fb.group({
-          label: item.label,
+          label: `control-${event.currentIndex}`,
+          name: `control-${event.currentIndex}`,
           type: item.type,
           labelView: item.labelView,
+          ...(item.type === 'radio' || item.type === 'checkbox'
+            ? {
+                controls: this.fb.array([
+                  this.fb.group({
+                    label: 'Radio 1',
+                    value: '1',
+                  }),
+                ]),
+              }
+            : {}),
         })
       );
     }
@@ -97,8 +111,31 @@ export class CreateFormComponent implements OnInit {
   }
 
   handleModalEven(data: any) {
+    this.openModal = false;
     console.log(data);
-    
+    if (data?.index >= 0) {
+      (this.form.controls['form'] as FormArray)
+        .at(data?.index)
+        .setValue(data.form);
+      (this.form as FormGroup).updateValueAndValidity();
+      console.log(this.form.value);
+    }
+  }
 
+  onEditControl(index: number) {
+    this.index = index;
+    this.selectedControl = (this.form.controls['form'] as FormArray).at(index);
+    this.openModal = true;
+  }
+
+  getControl(index: number) {
+    return (this.form.controls['form'] as FormArray).at(index) as FormGroup;
+  }
+
+  getControlArray(index: number) {
+    return (
+      ((this.form.controls['form'] as FormArray).at(index) as FormGroup)
+        .controls['controls'] as FormArray
+    ).controls as FormGroup[];
   }
 }
